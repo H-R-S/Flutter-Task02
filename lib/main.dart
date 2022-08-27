e9d1a96df2c48f17866024e985f3fb75bf4a8757
H-R-S <369haris@gmail.com>
2022-08-27 20:18:46 +0500

    Completed | Task02

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:task2/screens/chat/chat_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task 2',
+      home: ChatScreen(),
+    );
+  }
+}
